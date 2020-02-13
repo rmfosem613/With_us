d7825a638012ab0e29195b51b4f59940c6bd50d4
migyeong <rmfosem613@gmail.com>
2020-02-14 04:03:58 +0900

    search

---
+++ app/src/main/java/com/example/with_us/Adapter/UserAdapter.java
@@ -0,0 +1,80 @@
+package com.example.with_us.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.with_us.Model.User;
+import com.example.with_us.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUsers;
+
+    private FirebaseUser firebaseUser;
+
+    public UserAdapter(Context mContext, List<User> mUsers) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);  //4 영상 6:39
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int i) {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        final User user = mUsers.get(i);
+
+        viewHolder.btn_follow.setVisibility(View.VISIBLE);
+
+        viewHolder.username.setText(user.getUsername());
+        viewHolder.subject.setText(user.getSubject());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView username;
+        public TextView subject;
+        public CircleImageView image_profile;
+        public Button btn_follow;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            subject = itemView.findViewById(R.id.subject);
+            image_profile = itemView.findViewById(R.id.image_profile);
+            btn_follow = itemView.findViewById(R.id.btn_follow);
+
+        }
+
+    }
+}
