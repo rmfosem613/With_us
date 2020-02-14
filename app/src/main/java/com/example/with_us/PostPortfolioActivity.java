3c74b472a8c629c63686e07f4b793b74354c77cc
migyeong <rmfosem613@gmail.com>
2020-02-14 16:53:29 +0900

    modify

--- app/src/main/java/com/example/with_us/PostPortfolioActivity.java
+++ app/src/main/java/com/example/with_us/PostPortfolioActivity.java
@@ -1,14 +1,23 @@
 package com.example.with_us;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class PostPortfolioActivity extends AppCompatActivity {
 
@@ -17,12 +26,57 @@ public class PostPortfolioActivity extends AppCompatActivity {
     StorageTask uploadTask;
     StorageReference storageReference;
 
-    ImageView image_added;
-    TextView
+    ImageView image_added, close, post;
+    EditText description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_portfolio);
+
+        close = findViewById(R.id.close);
+        image_added = findViewById(R.id.image_added);
+        post = findViewById(R.id.post);
+        description = findViewById(R.id.description);
+
+        storageReference = FirebaseStorage.getInstance().getReference("posts");
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PostPortfolioActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+        CropImage.activity()
+                .setAspectRatio(3,4)
+                .start(PostPortfolioActivity.this);
+    }
+
+    private String getFileExtension (Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        ContentResolver.MimeTypeInfo mime =
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, @NonNull Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+        } else {
+            Toast.makeText(this, "오류가 났습니다!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(PostPortfolioActivity.this, MainActivity.class));
+            finish();
+        }
     }
 }
