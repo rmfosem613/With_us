0453d2ee254a7d8a55d8bfd35a8f1076ad5a3c7d
migyeong <rmfosem613@gmail.com>
2020-02-14 15:00:02 +0900

    modify

--- app/src/main/java/com/example/with_us/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/with_us/Fragment/ProfileFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.with_us.R;
 
@@ -17,8 +18,14 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile,container, false);
+
+
+        return view;
+
+
     }
+
+
 
 }
---
+++ app/src/main/java/com/example/with_us/PortfolioActivity.java
@@ -0,0 +1,14 @@
+package com.example.with_us;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PortfolioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portfolio);
+    }
+}
