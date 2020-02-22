289d372197b524fe958195543d1acd92c28f8a12
migyeong <rmfosem613@gmail.com>
2020-02-22 18:12:43 +0900

    modify

--- app/src/main/java/com/example/with_us/Fragment/EventDetailFragment.java
+++ app/src/main/java/com/example/with_us/Fragment/EventDetailFragment.java
@@ -1,4 +1,106 @@
 package com.example.with_us.Fragment;
 
-public class EventDetailFragment {
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.with_us.Adapter.EventAdapter;
+import com.example.with_us.Adapter.PostAdapter;
+import com.example.with_us.Model.Event;
+import com.example.with_us.Model.Post;
+import com.example.with_us.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EventDetailFragment extends Fragment {
+
+    String postid;
+    private RecyclerView recyclerView;
+    private EventAdapter eventAdapter;
+    private List<Event> postList;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_event_detail, container, false);
+
+
+        SharedPreferences preferences = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        postid = preferences.getString("postid", "none");
+
+        recyclerView = view.findViewById(R.id.recycler_view_event);
+//        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        postList = new ArrayList<>();
+        eventAdapter = new EventAdapter(getContext(), postList);
+        recyclerView.setAdapter(eventAdapter);
+
+
+        readPost();
+
+
+
+        return view;
+    }
+
+    private void readPost() {
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts").child(postid);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Events").getRef();
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // https://stackoverflow.com/questions/46429529/firebase-get-the-reference-of-the-child-of-a-child
+                boolean bOffset = true;
+                int offset = 0;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    //here is your every post
+                    String key = snapshot.getKey();
+                    Event value = snapshot.getValue(Event.class);
+
+                    postList.add(value);
+
+                    if (bOffset) {
+                        offset++;
+                    }
+
+                    if (postid.equals(value.getPostid())) {
+                        bOffset = false;
+                        Log.d("ddd", "height: " + recyclerView.computeHorizontalScrollOffset());
+                    }
+                }
+
+                eventAdapter.notifyDataSetChanged();
+                recyclerView.scrollToPosition(offset - 1);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/with_us/PostEventActivity.java
+++ app/src/main/java/com/example/with_us/PostEventActivity.java
@@ -1,14 +1,154 @@
 package com.example.with_us;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
 
 public class PostEventActivity extends AppCompatActivity {
+
+    Uri imageUri;
+    String myUri = "";
+    StorageTask uploadTask;
+    StorageReference storageReference;
+
+    ImageView event_added, close, post_event;
+    EditText eventtitle, eventdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_event);
+        setContentView(R.layout.activity_post_portfolio);
+
+        close = findViewById(R.id.close);
+        event_added = findViewById(R.id.event_added);
+        post_event = findViewById(R.id.post_event);
+        eventtitle = findViewById(R.id.eventtitle);
+        eventdate = findViewById(R.id.eventdate);
+
+        storageReference = FirebaseStorage.getInstance().getReference("events");
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PostEventActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+
+        post_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+        CropImage.activity()
+                .setAspectRatio(3,4)
+                .start(PostEventActivity.this);
+    }
+
+    private String getFileExtension (Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Posting");
+        progressDialog.show();
+
+        if(imageUri != null) {
+            final StorageReference filerefrence = storageReference.child(System.currentTimeMillis()
+                    + "."+ getFileExtension(imageUri));
+
+            uploadTask = filerefrence.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isComplete()) {
+                        throw task.getException();
+                    }
+                    return filerefrence.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        myUri = downloadUri.toString();
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+                        String postid = reference.push().getKey();
+
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("postid", postid);
+                        hashMap.put("postimage", myUri);
+                        hashMap.put("portfoliotitle", eventtitle.getText().toString());
+                        hashMap.put("portfoliodate", eventdate.getText().toString());
+                        hashMap.put("publisher", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                        reference.child(postid).setValue(hashMap);
+
+                        progressDialog.dismiss();
+
+                        startActivity(new Intent(PostEventActivity.this, MainActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(PostEventActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(PostEventActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(this, "No Image Selected!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, @NonNull Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+
+            event_added.setImageURI(imageUri);
+        } else {
+            Toast.makeText(this, "다시 시도해 주세요!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(PostEventActivity.this, MainActivity.class));
+            finish();
+        }
     }
 }
